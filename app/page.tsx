061b410aa5a73084fc00f230185d026759ae8d3e
Liv Asch <olivia.c.asch@gmail.com>
2024-01-03 14:37:03 +0000

    Remove test code from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,6 @@
 import { calculateStats } from '@/stats/calculateStats';
 import { fetchPlays } from '../bgg/fetchPlays';
 
-const Homepage = async () => {
-  const plays = await fetchPlays('olivvybee');
-
-  const stats = calculateStats({
-    plays,
-    startDate: '2023-01-01',
-    endDate: '2023-12-31',
-    username: 'olivvybee',
-    sortBy: 'plays',
-  });
-
-  return <pre>{JSON.stringify(stats, null, 2)}</pre>;
-};
+const Homepage = async () => <div>Cool app goes here</div>;
 
 export default Homepage;
